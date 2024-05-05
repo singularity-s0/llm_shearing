d816d7bfafc9cac09a13cb3498a203b1d9955183
singularity-s0 <12184989+singularity-s0@users.noreply.github.com>
2024-05-06 00:10:36 +0800

    update conversion script

--- llmshearing/scripts/100m_composer_to_hf.sh
+++ llmshearing/scripts/100m_composer_to_hf.sh
@@ -1,6 +1,6 @@
-MODEL_DIR=/remote-home/xjzhao/LLM_Shearing/llmshearing/ckpts/moss_2.5b_pruning_scaling_constant_to100m_sl4096_no_freeze_emb
-MODEL_PATH=$MODEL_DIR/pruned-latest-rank0.pt
-OUTPUT_PATH=$MODEL_DIR/hf-pruned-latest_rank0
+MODEL_DIR=/remote-home/xjzhao/LLM_Shearing/llmshearing/ckpts/finetune/continue_pretrain_100m_sl4096_ft48000ba_v3
+MODEL_PATH=$MODEL_DIR/latest-rank0.pt
+OUTPUT_PATH=$MODEL_DIR/hf-model
 MODEL_CLASS=Moss2ForCausalLM
 HIDDEN_SIZE=1024
 NUM_ATTENTION_HEADS=8
